94e3728e394a466018373db74e7250375c2564c2
OnlyF0uR <29165327+OnlyF0uR@users.noreply.github.com>
2024-10-26 20:22:21 +0200

    fix: macro + optimization

--- crates/cesium-material/src/keys.rs
+++ crates/cesium-material/src/keys.rs
@@ -1,16 +1,12 @@
+use crate::constants::NATIVE_TOKEN;
 use pqcrypto_sphincsplus::sphincsshake192ssimple::{PublicKey, SecretKey};
 use pqcrypto_traits::sign::{
     PublicKey as PqPublicKey, SecretKey as PqSecretKey, SignedMessage as PqSignedMessage,
 };
 
-use crate::constants::NATIVE_TOKEN;
-
 pub const PUB_BYTE_LEN: usize = 48;
 pub const SEC_BYTE_LEN: usize = 96;
 pub const SIG_BYTE_LEN: usize = 16224; // 35664 (192f), 16224 (192s)
-
-// pub const PUB_CHAR_LEN: usize = 96; (hex) would be ~66 for base58
-// pub const SEC_CHAR_LEN: usize = 192; (hex)
 
 pub type PublicKeyBytes = [u8; PUB_BYTE_LEN];
 pub type SecretKeyBytes = [u8; SEC_BYTE_LEN];
@@ -18,6 +14,15 @@ pub type SecretKeyBytes = [u8; SEC_BYTE_LEN];
 pub struct KeyPair {
     public_key: PublicKey,
     secret_key: Option<SecretKey>,
+}
+
+macro_rules! ensure_secret_key {
+    ($self:expr) => {
+        match $self.secret_key.as_ref() {
+            Some(sk) => sk,
+            None => return Err("Secret key is missing".into()),
+        }
+    };
 }
 
 impl KeyPair {
@@ -36,10 +41,7 @@ impl KeyPair {
             return Err("Invalid public key length".into());
         }
         let public_key = PublicKey::from_bytes(&pk_bytes)?;
-        Ok(KeyPair {
-            public_key,
-            secret_key: None,
-        })
+        Ok(KeyPair::readonly_from_pub(public_key))
     }
 
     pub fn create() -> KeyPair {
@@ -50,22 +52,12 @@ impl KeyPair {
         }
     }
 
-    // Idea: Use detached hash instead where the specified buffer,
-    // must have the size of the SIG_BYTE_LEN + the message byte length
-    // to continue using slices
     pub fn digest(
         &self,
         message: &[u8],
     ) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
-        if self.secret_key.is_none() {
-            return Err("Secret key is missing".into());
-        }
-
-        let sm = pqcrypto_sphincsplus::sphincsshake192ssimple_sign(
-            message,
-            &self.secret_key.as_ref().unwrap(),
-        );
-
+        let sk = ensure_secret_key!(self);
+        let sm = pqcrypto_sphincsplus::sphincsshake192ssimple_sign(message, sk);
         Ok(sm.as_bytes().to_vec())
     }
 
@@ -74,9 +66,8 @@ impl KeyPair {
         message: &[u8],
         signature: &[u8],
     ) -> Result<bool, Box<dyn std::error::Error + Send + Sync>> {
-        let tl = signature.len() - message.len();
-        if tl != SIG_BYTE_LEN {
-            return Err("Incongruent message and/or signature length".into());
+        if signature.len() < message.len() + SIG_BYTE_LEN {
+            return Err("Invalid signature length".into());
         }
 
         let sm = PqSignedMessage::from_bytes(signature)?;
@@ -85,26 +76,20 @@ impl KeyPair {
         Ok(verified_message == message)
     }
 
-    pub fn secret_key(&self) -> Result<SecretKey, Box<dyn std::error::Error + Send + Sync>> {
-        if self.secret_key.is_none() {
-            return Err("Secret key is missing".into());
-        }
-        Ok(self.secret_key.as_ref().unwrap().clone())
+    pub fn secret_key(&self) -> Result<&SecretKey, Box<dyn std::error::Error + Send + Sync>> {
+        Ok(ensure_secret_key!(self))
     }
 
-    pub fn public_key(&self) -> PublicKey {
-        self.public_key.clone()
+    pub fn public_key(&self) -> &PublicKey {
+        &self.public_key
     }
 
     pub fn from_bytes(
         public_key_bytes: &[u8],
         secret_key_bytes: &[u8],
     ) -> Result<KeyPair, Box<dyn std::error::Error + Send + Sync>> {
-        if public_key_bytes.len() != PUB_BYTE_LEN {
-            return Err("Invalid public key length".into());
-        }
-        if secret_key_bytes.len() != SEC_BYTE_LEN {
-            return Err("Invalid secret key length".into());
+        if public_key_bytes.len() != PUB_BYTE_LEN || secret_key_bytes.len() != SEC_BYTE_LEN {
+            return Err("Invalid key length".into());
         }
 
         let public_key = PublicKey::from_bytes(public_key_bytes)?;
@@ -122,14 +107,6 @@ impl KeyPair {
         let pk_bytes = bs58::decode(public_key_s).into_vec()?;
         let sk_bytes = hex::decode(secret_key_s)?;
 
-        if pk_bytes.len() != PUB_BYTE_LEN {
-            return Err("Invalid public key length".into());
-        }
-
-        if sk_bytes.len() != SEC_BYTE_LEN {
-            return Err("Invalid secret key length".into());
-        }
-
         KeyPair::from_bytes(&pk_bytes, &sk_bytes)
     }
 
@@ -137,12 +114,7 @@ impl KeyPair {
         &self,
     ) -> Result<(&PublicKeyBytes, &SecretKeyBytes), Box<dyn std::error::Error + Send + Sync>> {
         let pk = slice_to_array_48(self.public_key.as_bytes())?;
-
-        if self.secret_key.is_none() {
-            return Err("Secret key is missing, use to_public_key_bytes instead".into());
-        }
-
-        let sk = slice_to_array_96(self.secret_key.as_ref().unwrap().as_bytes())?;
+        let sk = slice_to_array_96(ensure_secret_key!(self).as_bytes())?;
         Ok((pk, sk))
     }
 
@@ -150,24 +122,13 @@ impl KeyPair {
         &self,
     ) -> Result<(String, String), Box<dyn std::error::Error + Send + Sync>> {
         let pk_s = bs58::encode(self.public_key.as_bytes()).into_string();
-
-        if self.secret_key.is_none() {
-            return Err("Secret key is missing, use to_public_key_readable instead".into());
-        }
-
-        let sk_s = hex::encode(self.secret_key.as_ref().unwrap().as_bytes());
+        let sk_s = hex::encode(ensure_secret_key!(self).as_bytes());
         Ok((pk_s, sk_s))
     }
 
     pub fn to_public_key_bytes(&self) -> &PublicKeyBytes {
-        let bytes = self.public_key.as_bytes();
-        match slice_to_array_48(bytes) {
-            Ok(formatted) => formatted,
-            // Yes there is a panic here, but this should never happen
-            // because a public key is only accepted if the length is indeed
-            // 48 bytes
-            Err(_) => panic!("Invalid public key length"),
-        }
+        slice_to_array_48(self.public_key.as_bytes())
+            .expect("Invalid public key length; expected 48 bytes")
     }
 
     pub fn to_public_key_readable(&self) -> String {
@@ -178,30 +139,23 @@ impl KeyPair {
 pub fn address_to_bytes(
     address: &str,
 ) -> Result<PublicKeyBytes, Box<dyn std::error::Error + Send + Sync>> {
-    if address == NATIVE_TOKEN {
-        // We can skip the last few bytes
-        let mut bytes = NATIVE_TOKEN.as_bytes().to_vec();
-        println!("bytes: {:?}", bytes);
-        println!("bytes.len(): {:?}", bytes.len());
-        bytes.truncate(PUB_BYTE_LEN);
-
-        let formatted = slice_to_array_48(&bytes.as_slice())?;
-        return Ok(formatted.clone());
-    }
-
-    let bytes = bs58::decode(address).into_vec()?;
-    let formatted = slice_to_array_48(&bytes.as_slice())?;
-
-    Ok(formatted.clone())
+    let bytes = if address == NATIVE_TOKEN {
+        let mut native_bytes = NATIVE_TOKEN.as_bytes().to_vec();
+        native_bytes.truncate(PUB_BYTE_LEN);
+        native_bytes
+    } else {
+        bs58::decode(address).into_vec()?
+    };
+    slice_to_array_48(&bytes).map(|arr| *arr)
 }
+
 pub fn sig_byte_len(msg_len: usize) -> usize {
     SIG_BYTE_LEN + msg_len
 }
 
 fn slice_to_array_48<T>(slice: &[T]) -> Result<&[T; 48], Box<dyn std::error::Error + Send + Sync>> {
     if slice.len() == 48 {
-        let ptr = slice.as_ptr() as *const [T; 48];
-        unsafe { Ok(&*ptr) }
+        Ok(unsafe { &*(slice.as_ptr() as *const [T; 48]) })
     } else {
         Err("Invalid slice length".into())
     }
@@ -209,8 +163,7 @@ fn slice_to_array_48<T>(slice: &[T]) -> Result<&[T; 48], Box<dyn std::error::Err
 
 fn slice_to_array_96<T>(slice: &[T]) -> Result<&[T; 96], Box<dyn std::error::Error + Send + Sync>> {
     if slice.len() == 96 {
-        let ptr = slice.as_ptr() as *const [T; 96];
-        unsafe { Ok(&*ptr) }
+        Ok(unsafe { &*(slice.as_ptr() as *const [T; 96]) })
     } else {
         Err("Invalid slice length".into())
     }
